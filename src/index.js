140420039cdf372a42ba4d850f036df05d8cf6f1
whdjh <whdjh98@naver.com>
2024-09-27 02:02:25 +0900

    Apply Btn func & Print result value

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
 import ReactDOM from 'react-dom/client';
 
+const WINS = {
+  rock: 'scissor',
+  scissor: 'paper',
+  paper: 'rock',
+};
+
+function getResult(left, right) {
+  if (WINS[left] === right) return '승리';
+  else if (left === WINS[right]) return '패배';
+  return '무승부';
+}
+
+function handleClick() {
+  console.log('가위바위보!');
+}
+
+const me = 'rock';
+const other = 'scissor';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
+<>
+  <h1 id="title">가위바위보</h1>
+  <h2>{getResult(me, other)}</h2>
+  <button onClick={handleClick} className='hand'>가위</button>
+  <button onClick={handleClick} className='hand'>바위</button>
+  <button onClick={handleClick} className='hand'>보</button>
+</>
 );
